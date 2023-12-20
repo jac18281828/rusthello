7ea41a58926e46927cecd2e5c252d354dc62c1ed
John Cairns <john@2ad.com>
2023-12-20 17:00:36 -0600

    quad toy
    remove target

---
+++ quadratic/src/main.rs
@@ -0,0 +1,38 @@
+extern crate zksnark;
+
+use zksnark::groth16;
+use zksnark::groth16::{Proof, SigmaG1, SigmaG2, QAP};
+use zksnark::groth16::circuit::{ASTParser, TryParse};
+use zksnark::groth16::fr::FrLocal;
+use zksnark::groth16::coefficient_poly::CoefficientPoly;
+
+fn main() {
+// y = a*x*x + b*x + c
+let code = &*::std::fs::read_to_string("program/simple.zk").unwrap();
+let qap: QAP<CoefficientPoly<FrLocal>> =
+    ASTParser::try_parse(code)
+        .unwrap()
+        .into();
+
+// The assignments are the inputs to the circuit in the order they
+// appear in the file
+let assignments = &[
+    3.into(), // a
+    2.into(), // b
+    4.into(), // c
+];
+
+let weights = groth16::weights(code, assignments).unwrap();
+
+let (sigmag1, sigmag2) = groth16::setup(&qap);
+
+let proof = groth16::prove(&qap, (&sigmag1, &sigmag2), &weights);
+
+assert!(groth16::verify(
+    &qap,
+    (sigmag1, sigmag2),
+    &vec![FrLocal::from(2), FrLocal::from(34)],
+    proof
+));
+
+}
